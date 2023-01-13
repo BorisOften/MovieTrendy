f419d5ccc586a1267e015f894e7a88ebd52ff2ab
Boris <banofon@gmail.com>
2023-01-13 17:40:39 -0600

    Create the project

---
+++ MovieTrending/MovieTrending/MainViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MovieTrending
+//
+//  Created by Boris Ofon on 1/13/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
